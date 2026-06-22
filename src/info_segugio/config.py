1683ec6a886ce9fd233a6d64c732c1d46e37f05d
Giuliana Tecchio <giulianatcch@gmail.com>
2026-06-22 13:06:29 +0200

    aggiunto

--- src/info_segugio/config.py
+++ src/info_segugio/config.py
@@ -5,5 +5,6 @@ class Config:
     LLM_MODEL = "gpt-4o"
     LLM_MODEL_LOW = "gpt-4o-mini"
     AI_API_URL = "https://api.openai.com/v1/"
-    AI_API_KEY = "inserisci_qui_la_tua_chiave"
+    AI_API_KEY = os.environ.get("OPENAI_API_KEY")
+    TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
 
